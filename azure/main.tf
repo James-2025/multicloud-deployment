c52be983a001c12cde704da38db131b3c8feb63a
James-2025 <4al20cs052@gmail.com>
2024-11-04 13:38:49 +0530

    c08

--- azure/main.tf
+++ azure/main.tf
@@ -12,28 +12,6 @@ provider "azurerm" {
 }
 
 
-variable "vm_name" {
-  default = "my-azure-vm01"
-}
-
-variable "ssh_public_key" {
-  description = "The SSH public key used for authentication."
-  type        = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFSz/3f6ccuQsSaMjasW9P+NzYjVaokcqDdZ7nh6n3VlQFCLRBMTIjj82ohQm1bbdlTd38mmhOm8CThar4rfXnnF0UxNhTK41LDp678wepxTK0MkbeaQpgOMdSAOolGvPtKxppbrk7KK65+zeH2J/1pH+4bkwn3kaagnGtZir0uOJRBFAJAUNACplWEbTtYdNQ+TNn7DNeXu9ew+949953bWCVubRUVia+sdF0HPFqMzeGPNZL3PxZMs+uCzNl952pj+wSqOyMMK15+UoPMuLX+KT3iRjigeugaAxSdnhkBVH734DEjk3qb6wpGtFv58zfwbwrVSowMbxTMJyzpc4I9Loq4yuapd6utLGshUSALdUUbCdCFcp+IoDNqFvuSSKRpmPrYJnicrPzKVpLqtwfuRk12p07c5jSIgX7Y0DC5GQi4MNqyfBJVCki8u++vlmLrxAxVcxmNJGQLL2bOuvfYbI0Xd+FzjRadilBoWUUHzloQC/4DnGVGBc6WXokezE= generated-by-azure"
-}
-
-
-variable "resource_group_name" {
-  default = "my-terraform-rg"
-}
-
-variable "location" {
-  default = "westus2"
-}
-
-variable "admin_username" {
-  default = "azureuser"
-}
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.vm_name}-nsg"
@@ -54,6 +32,21 @@ resource "azurerm_network_security_rule" "ssh" {
   network_security_group_name = azurerm_network_security_group.nsg.name
   resource_group_name         = "my-terraform-rg"  # Add this line
 
+}
+
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "allow-http"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = "my-terraform-rg"  # Add this line
 }
 
 
--- azure/variables.tf
+++ azure/variables.tf
@@ -3,3 +3,26 @@ variable "private_key_path" {
   type        = string
   default = "~/.ssh/2024.pem"
 }
+
+variable "vm_name" {
+  default = "my-azure-vm01"
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key used for authentication."
+  type        = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFSz/3f6ccuQsSaMjasW9P+NzYjVaokcqDdZ7nh6n3VlQFCLRBMTIjj82ohQm1bbdlTd38mmhOm8CThar4rfXnnF0UxNhTK41LDp678wepxTK0MkbeaQpgOMdSAOolGvPtKxppbrk7KK65+zeH2J/1pH+4bkwn3kaagnGtZir0uOJRBFAJAUNACplWEbTtYdNQ+TNn7DNeXu9ew+949953bWCVubRUVia+sdF0HPFqMzeGPNZL3PxZMs+uCzNl952pj+wSqOyMMK15+UoPMuLX+KT3iRjigeugaAxSdnhkBVH734DEjk3qb6wpGtFv58zfwbwrVSowMbxTMJyzpc4I9Loq4yuapd6utLGshUSALdUUbCdCFcp+IoDNqFvuSSKRpmPrYJnicrPzKVpLqtwfuRk12p07c5jSIgX7Y0DC5GQi4MNqyfBJVCki8u++vlmLrxAxVcxmNJGQLL2bOuvfYbI0Xd+FzjRadilBoWUUHzloQC/4DnGVGBc6WXokezE= generated-by-azure"
+}
+
+
+variable "resource_group_name" {
+  default = "my-terraform-rg"
+}
+
+variable "location" {
+  default = "westus2"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
